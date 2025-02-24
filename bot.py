9199c0c7a8c9e584c55e6d444aa4ea13122fa1f2
zoro4all <solo.zoro4all@gmail.com>
2025-02-24 10:38:18 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,10 @@ from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
+import pyrogram.utils
 
+pyrogram.utils.MIN_CHANNEL_ID = -1002373140882
+pyrogram.utils.MIN_CHANNEL_ID = -1002421037505
 ascii_art = """
 ░█████╗░░█████╗░██████╗░███████╗██╗░░██╗██████╗░░█████╗░████████╗███████╗
 ██╔══██╗██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗╚══██╔══╝╚════██║
